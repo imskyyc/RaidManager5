00d83952f935c4856979ae72b3237fdb2e35767b
Jaiden <sky@imskyyc.com>
2023-09-01 19:48:35 -0700

    refactor verification command

--- src/api/routes/verification-callback.get.ts
+++ src/api/routes/verification-callback.get.ts
@@ -20,89 +20,93 @@ export default async (guardsman: Guardsman, request: Request, response: Response
     client_secret: guardsman.environment.ROBLOX_CLIENT_TOKEN,
     grant_type: "authorization_code",
     code: oauthCode.toString()
-  }, { headers: {"Content-Type": "application/x-www-form-urlencoded"} })
-  .then((res) => {
-    const access_token = res.data.access_token;
+  }, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  })
+    .then(res =>
+    {
+      const accessToken = res.data.access_token;
+      if (!accessToken) return response.json({success: "false", status: "OAuth2 access token not sent on response. Please try again."});
 
-    if (!access_token) return response.json({success: "false", status: "OAuth2 access token not sent on response. Please try again."});
+      axios.get("https://apis.roblox.com/oauth/v1/userinfo", {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Authorization": "Bearer " + accessToken
+        },
 
-    axios.get("https://apis.roblox.com/oauth/v1/userinfo", {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": "Bearer " + access_token
-      },
+        data: {
+          client_id: guardsman.environment.ROBLOX_CLIENT_ID,
+          client_secret: guardsman.environment.ROBLOX_CLIENT_TOKEN
+        }
+      })
+        .then(async res =>
+        {
+          const userData = res.data;
 
-      data: {
-        client_id: guardsman.environment.ROBLOX_CLIENT_ID,
-        client_secret: guardsman.environment.ROBLOX_CLIENT_TOKEN
-      }
+          const userId = userData.sub;
+          const username = userData.preferred_username;
+
+          const pendingVerificationUser = await guardsman.database<IVerificationConfirmation>("pending_verification").where({
+            token: codeState.toString()
+          }).first();
+
+          if (!pendingVerificationUser) return response.json({
+            success: false,
+            status: "Unable to verify request. Please try again."
+          });
+
+          const existingUser = await guardsman.database<IUser>("users")
+              .select("*")
+              .where("username", username)
+              .orWhere("roblox_id", userId)
+              .orWhere("discord_id", pendingVerificationUser.discord_id)
+              .first();
+
+          if (existingUser) {
+            await guardsman.database<IUser>("users")
+                .update({
+                  username: username,
+                  roblox_id: userId,
+                  discord_id: pendingVerificationUser.discord_id,
+                })
+                .where({
+                  id: existingUser.id
+                })
+          } else {
+            await guardsman.database<IUser>("users")
+                .insert({
+                  username: username,
+                  roblox_id: userId,
+                  discord_id: pendingVerificationUser.discord_id,
+                  roles: "[\"Player\"]"
+                })
+          }
+
+          await guardsman.database<IVerificationConfirmation>("pending_verification")
+              .delete("*")
+              .where({
+                discord_id: pendingVerificationUser.discord_id
+              })
+
+          response.redirect("https://guardsman.bunkerbravointeractive.com/verification-complete")
+
+          guardsman.bot.emit("verificationComplete", pendingVerificationUser.discord_id, username.toString());
+        })
+        .catch(_ =>
+        {
+          return response.json({
+            success: "false",
+            status: "An error occurred when fetching OAuth2 resources. Please try again."
+          })
+        })
     })
-    .then(async userInfo => {
-      const userData = userInfo.data;
-
-      const userId = userData.sub;
-      const username = userData.preferred_username;
-
-      const pendingVerificationUser = await guardsman.database<IVerificationConfirmation>("pending_verification").where({
-        token: codeState.toString()
-      }).first();
-
-      if (!pendingVerificationUser) return response.json({
-        success: false,
-        status: "Unable to verify request. Please try again."
-      });
-
-      const existingUser = await guardsman.database<IUser>("users")
-        .select("*")
-        .where("username", username)
-        .orWhere("roblox_id", userId)
-        .orWhere("discord_id", pendingVerificationUser.discord_id)
-        .first();
-
-      if (existingUser) {
-        await guardsman.database<IUser>("users")
-        .update({
-          username: username,
-          roblox_id: userId,
-          discord_id: pendingVerificationUser.discord_id,
-        })
-        .where({
-          id: existingUser.id
-        })
-      } else {
-        await guardsman.database<IUser>("users")
-        .insert({
-          username: username,
-          roblox_id: userId,
-          discord_id: pendingVerificationUser.discord_id,
-          roles: "[\"Player\"]"
-        })
-      }
-
-      await guardsman.database<IVerificationConfirmation>("pending_verification")
-        .delete("*")
-        .where({
-          discord_id: pendingVerificationUser.discord_id
-        })
-
-      response.redirect("https://guardsman.bunkerbravointeractive.com/verification-complete")
-
-      guardsman.bot.emit("verificationComplete", pendingVerificationUser.discord_id, username.toString());
-    })
-    .catch(err => {
-      console.log(err);
-
+    .catch(_ =>
+    {
       return response.json({
         success: "false",
-        status: "An error occurred when fetching OAuth2 resources. Please try again."
+        status: "OAuth2 code is invalid. Please try again."
       })
-    })
-  })
-  .catch((res) => {
-    console.log(res);
-    return response.json({
-      success: "false",
-      status: "OAuth2 code is invalid. Please try again."
-    })
-  })
+    });
 }
