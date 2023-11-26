aa3616a9333c8fe2ffb4538b7f835a32b0bd514a
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2023-11-25 22:34:50 -0600

    if user clears the embed it just dies

--- src/bot/events/verificationComplete.ts
+++ src/bot/events/verificationComplete.ts
@@ -3,24 +3,26 @@ import { Guardsman } from "index";
 
 export default async (guardsman: Guardsman, discordId: string) =>
 {
-    const interaction = guardsman.bot.pendingVerificationInteractions[discordId];
+    try {
+        const interaction = guardsman.bot.pendingVerificationInteractions[discordId];
 
-    const reply = await interaction.editReply({
-        components: [],
+        const reply = await interaction.editReply({
+            components: [],
+    
+            embeds: [
+            new EmbedBuilder()
+                .setTitle(`L.I.A.A.S Verification`)
+                .setDescription(
+                    "Discord account verification was successful! Please run `/update` to obtain roles."
+                )
+                .setColor(Colors.Green)
+                .setTimestamp()
+                .setFooter({
+                    text: "L.I.A.A.S Verification"
+                }),
+            ],
+        });
+    } catch (error) {}
 
-        embeds: [
-        new EmbedBuilder()
-            .setTitle(`RaidManager Verification`)
-            .setDescription(
-                "Discord account verification was successful! Please run `/update` to obtain roles."
-            )
-            .setColor(Colors.Green)
-            .setTimestamp()
-            .setFooter({
-                text: "RaidManager Verification"
-            }),
-        ],
-    });
-
-    await reply.reply(`<@${interaction.member.id}>`)
+    //await reply.reply(`<@${interaction.member.id}>`) --//Because it is already replied to?
 }
