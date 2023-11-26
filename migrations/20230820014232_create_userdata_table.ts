0b6270e1e9703769f547ef248b964363c6da7ee1
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2023-11-25 22:47:25 -0600

    fix for migrations

--- migrations/20230820014232_create_userdata_table.ts
+++ migrations/20230820014232_create_userdata_table.ts
@@ -11,7 +11,8 @@ export async function up(knex: Knex): Promise<void> {
         table.json("squadron_medals").notNullable().defaultTo([]);
         table.timestamp("squadron_loa_start_date");
 
-        table.timestamps();
+        table.dateTime('created_at').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP'));
+        table.dateTime('updated_at').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
     })
 }
 
